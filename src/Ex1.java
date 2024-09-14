bac8429bc323df12f1c206ae69c4f75585c82af4
Emerson <euemerson1@gmail.com>
2024-09-14 10:30:33 -0300

    first commit

---
+++ src/Ex1.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        LinkedList ToDoTask = new LinkedList();
+
+        boolean opc =true;
+
+        while(opc){
+            System.out.println("ToDoLIst");
+        }
+    }
+}
